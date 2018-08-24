f5c5256b529e15dd1e356342127d9491064e96d4
Ana M <ana.alyse13@gmail.com>
2018-08-24 15:58:07 -0500

    final improved code

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
 var html = '';
-var red;
-var green;
-var blue;
 var rgbColor;
 
-for ( var i = 0; i <= 10; i += 1 ) {
-  red = Math.floor(Math.random() * 256 );
-  green = Math.floor(Math.random() * 256 );
-  blue = Math.floor(Math.random() * 256 );
-  rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+function randomRGB() {
+  return Math.floor(Math.random() * 256 );
+}
+
+function randomColor() { /* creating a function means it can be used elsewhere, outside the for loop */
+  var color = 'rgb(';
+  color += randomRGB() + ','; /* calls the randomRGB function, adds a comma, then adds that to the color variable */
+  color += randomRGB() + ',';
+  color += randomRGB() + ')';
+  return color;
+}
+
+function print(message) {
+  document.write(message);
+}
+
+for ( var i = 0; i <= 1000; i += 1 ) {
+  rgbColor = randomColor();
   html += '<div style="background-color:' + rgbColor + '"></div>';
 }
-document.write(html);
+print(html);
